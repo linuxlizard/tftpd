fd84f8e5622f616f658897cf024fdb157505daa0
David Poole <dpoole@cradlepoint.com>
2018-04-30 14:42:33 -0600

    my tftp server in python2 copied from old backups
    TODO convert to python3
    TODO update to newer TFTP standards (esp. blksize)

---
+++ tftp.py
@@ -0,0 +1,234 @@
+#!/usr/bin/python
+
+import struct
+
+UDP_PORT = 69
+
+RRQ = 1   # Read request
+WRQ = 2   # Write request
+DATA = 3  
+ACK = 4   
+ERROR = 5 
+
+opcode_strings = ( "(unknown)", "RRQ", "WRQ", "DATA", "ACK", "ERROR" )
+opcode_names = ( "(unknown)", "Read Request", "Write Request", "Data", "Acknowledgement", "Error" )
+
+#Error Codes
+#   Value	 Meaning
+#   0		 Not defined, see error message (if any).
+#   1		 File not found.
+#   2		 Access violation.
+#   3		 Disk full or allocation exceeded.
+#   4		 Illegal TFTP operation.
+#   5		 Unknown transfer ID.
+#   6		 File already exists.
+#   7		 No such user.
+ERROR_UNDEFINED = 0
+ERROR_NO_SUCH_FILE = 1
+ERROR_ACCESS_VIOLATION = 2
+ERROR_DISK_FULL = 3
+ERROR_ILLEGAL_TFTP_OP = 4
+ERROR_UNKNOWN_TRANSFER_ID = 5
+ERROR_FILE_EXISTS = 6
+ERROR_NO_SUCH_USER = 7
+
+# useful for debugging
+def hexdump(str) :
+	for c in str :
+		print "%02x" % ord(c),
+	print
+
+class packet_error :
+	pkt = ""
+	errmsg = ""
+	def __init__( self, pkt, errmsg ) :
+		self.pkt = pkt
+		self.errmsg = errmsg
+
+	def __str__( self ) :
+		return "Packet error : %s" % self.errmsg
+
+class Packet :
+	op = 0
+	packet = ""   # raw, encoded packet, ready to be sent onto the network
+
+	def __init__( self, buffer="" ) :
+		self.packet = buffer
+
+	def __str__( self ) :
+		str = opcode_strings[self.op]
+		return str
+
+class Request( Packet ) :
+	filename = ""
+	mode = ""
+	
+	def __init__( self, filename="", mode="" ) :
+		Packet.__init__( self )
+		self.filename = filename
+		self.mode = mode.lower()
+
+	def pack( self ) :
+		self.packet = struct.pack( ("!H%dsc%dsc"%(len(self.filename),len(self.mode))), \
+				self.op, self.filename, "\x00", self.mode, "\x00" )
+
+	def unpack( self ) :
+		"""Parse a WRQ or RRQ, verifying pkt, extracting filename and mode."""
+		self.op,pkt = struct.unpack( ("!H%ds" % ( len(self.packet)-2)), self.packet )
+		fields = pkt.split( '\x00' )
+		print(fields)
+		# there are two fields, but the split will give us three with the empty
+		# string after the final NULL
+#		if len(fields) != 3 or len(fields[2]) != 0 :
+#			raise packet_error( pkt, ("Bad %s (%s); couldn't find the filename and mode fields." \
+#					% (opcode_strings[self.op],opcode_names[self.op]) ) )
+		self.filename = fields[0]
+		self.mode = fields[1].lower()
+
+	def __str__( self ) :
+		str = Packet.__str__( self )
+		str = str + " filename=%s mode=%s" % (self.filename,self.mode)
+		return str
+
+class ReadRequest( Request ) :
+	def __init__( self, filename="", mode="" ) :
+		Request.__init__( self, filename, mode )
+		self.op = RRQ
+
+class WriteRequest( Request ) :
+	def __init__( self, filename="", mode="" ) :
+		Request.__init__( self, filename, mode )
+		self.op = WRQ
+
+class Ack( Packet ) :
+	block_num = 0
+
+	def __init__( self, block_num=0 ) :
+		Packet.__init__( self )
+		self.op = ACK
+		self.block_num = block_num
+
+	def pack( self ) :
+		self.packet = struct.pack( "!HH", self.op, self.block_num )
+
+	def unpack( self ) :
+		"""Parse an ACK, verifying pkt, extracting block number."""
+		if len(self.packet) < 4 :
+			raise packet_error( self.packet, "Packet too small (len=%d)."%len(self.packet) )
+		self.op,self.block_num,junk = struct.unpack( ("!HH%ds" % (len(self.packet)-4) ), self.packet )
+
+	def __str__( self ) :
+		str = Packet.__str__( self )
+		str = str + " block=%d" % self.block_num
+		return str
+
+class Data( Packet ) :
+	block_num = 0
+	data = ""
+
+	def __init__( self, data="" ) :
+		Packet.__init__( self )
+		self.op = DATA 
+		self.data = data
+		self.block_num = 0
+	
+	def pack( self ) :
+		assert len(self.data) <= 512
+		self.packet = struct.pack( ("!HH%ds"%len(self.data)), self.op, self.block_num, self.data )
+
+	def unpack( self ) :
+		"""Parse a DATA, verifying pkt, extracting data field and block number."""
+		if len(self.packet) > 516 :
+			raise packet_error( self.packet, "Packet too large (len=%d)."%len(self.packet) )
+		self.op,self.block_num,self.data = struct.unpack( ("!HH%ds" % (len(self.packet)-4)), self.packet )
+
+	def __str__( self ) :
+		str = Packet.__str__(self)
+		str = str + " block=%d datalen=%d" % (self.block_num,len(self.data))
+		return str
+
+class Error( Packet ) :
+	error_code = 0
+	error_msg = ""
+
+	def __init__( self, error_code=0, error_msg="" ) :
+		Packet.__init__( self )
+		self.op = ERROR
+		self.error_code = error_code
+		self.error_msg = error_msg
+
+	def pack( self ) :
+		self.packet = struct.pack( ("!HH%dsc"%len(self.error_msg)), self.op, self.error_code, self.error_msg, "\x00" )
+
+	def unpack( self ) :
+		self.op,self.error_code,error_msg = struct.unpack( ("!HH%ds" % (len(self.packet)-4)), self.packet )
+		# take up to the first NULL as the error message
+		self.error_msg = error_msg.split( "\x00" ) [0]
+		
+	def __str__( self ) :
+		str = Packet.__str__(self)
+		str = str + " error_code=%d error_msg=\"%s\"" % (self.error_code, self.error_msg)
+		return str
+
+debugging = 0
+
+def parse( buffer ) :
+	"""A class factory of sorts.  Take a raw buffer, partially parse it, then 
+	instantiate and return a new class for that packet."""
+
+	op,pkt = struct.unpack( ("!H%ds" % ( len(buffer)-2)), buffer )
+
+	if op == RRQ :
+		pkt = ReadRequest()
+	elif op == WRQ :
+		pkt = WriteRequest()
+	elif op == DATA :
+		pkt = Data()
+	elif op == ACK :
+		pkt = Ack()
+	elif op == ERROR :
+		pkt = Error()
+	else :
+		raise packet_error( packet, ("Bad opcode %02x in packet" % op) )
+
+	pkt.packet = buffer
+	pkt.unpack()
+
+	if debugging :
+		print pkt
+	return pkt
+
+def __pack_test( pkt ) :
+	pkt.pack()
+	pkt.unpack()
+	pkt.pack()
+	print pkt
+	print parse( pkt.packet )
+
+if __name__ == '__main__' :
+
+	rd = ReadRequest()
+	__pack_test( rd )
+	rd = ReadRequest( "/etc/foo/bar", "octet" )
+	__pack_test( rd )
+
+	wr = WriteRequest()
+	__pack_test( wr )
+	wr = WriteRequest( "/etc/foo/bar", "octet" )
+	__pack_test( wr )
+
+	ack = Ack()
+	__pack_test( ack )
+	ack = Ack( 99 ) 
+	__pack_test( ack )
+
+	data = Data()
+	__pack_test( data )
+	data = Data( "This is a test" )
+	__pack_test( data )
+
+	err = Error()
+	__pack_test( err )
+	err = Error( ERROR_FILE_EXISTS, "Foo! Bar! Baz!" )
+	__pack_test( err )
+
---
+++ tftpd.py
@@ -0,0 +1,254 @@
+#!/usr/bin/python
+
+import socket
+import SocketServer
+import select
+
+import tftp
+import cvtfile
+
+data_retry_timeout = 2.0 # seconds
+data_retry_max = 3  # resend DATA packets this many times before giving up
+
+class Error(Exception) : pass
+
+class internal_error(Error) : pass
+
+class Nettftpd( SocketServer.BaseRequestHandler ) :
+	data_sock = None
+	debugging = 2
+
+	def set_debuglevel(self, level):
+		# took this from ftplib.py
+		'''Set the debugging level.
+		The required argument level means:
+		0: no debugging output (default)
+		1: print commands and responses but not body text etc.
+		2: also print raw lines read and sent before stripping CR/LF'''
+		self.debugging = level
+
+	def send( self, pkt ) :
+		if self.debugging :
+			print pkt
+		pkt.pack()
+		self.data_sock.sendto( pkt.packet, self.client_address )
+
+	def pathcheck( self, tftppkt ) :
+		# if trying to get any file with a path component, slap 'em
+		# down and refuse
+		if tftppkt.filename.find( "/" ) >= 0 :
+			print "Filename \"%s\" with path component rejected." % tftppkt.filename
+			errpkt = tftp.Error( tftp.ERROR_ACCESS_VIOLATION, "Ignorning filename with path." )
+			self.send( errpkt )
+			return 0
+
+		return 1
+
+	def filemodecheck( self, tftppkt ) :
+		if tftppkt.mode != "octet" :
+			print "Invalid file mode \"%s\"." % tftppkt.mode
+			errpkt = tftp.Error( tftp.ERROR_ILLEGAL_TFTP_OP, "Only octet mode is supported." )
+			self.send( errpkt )
+			return 0
+
+#		if tftppkt.mode != "netascii" and tftppkt.mode != "octet" :
+#			print "Invalid file mode \"%s\"." % tftppkt.mode
+#			errpkt = tftp.Error( tftp.ERROR_ILLEGAL_TFTP_OP, "Only netascii and octet mode are supported." )
+#			self.send( errpkt )
+#			return 0
+
+		return 1
+
+	def write_request( self, tftppkt ) :
+		assert tftppkt.filename
+		assert tftppkt.mode
+
+		self.data_sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+		self.data_sock.bind( ("",0) )
+
+		if not self.pathcheck( tftppkt ) :
+			self.data_sock.close()
+			return
+
+		if not self.filemodecheck( tftppkt ) :
+			self.data_sock.close()
+			return
+
+		# TODO disable writing to files and writing to existing files
+		try :
+			if tftppkt.mode == "netascii" :
+				file = cvtfile.UnixToDosFile()
+				file.open( tftppkt.filename, "wb" )
+			else :
+				file = open( tftppkt.filename, "wb" )
+		except IOError, err :
+			print "Failed to open %s : " % tftppkt.filename, err
+			errpkt = tftp.Error( tftp.ERROR_NO_SUCH_FILE, err.strerror )
+			self.send( errpkt )
+			self.data_sock.close()
+			return
+
+		ackpkt = tftp.Ack()
+		self.send(ackpkt)
+		next_block_num = 1
+
+		# wait for the DATA; resend ACK if we don't get data
+		retry_count = 0
+		last = 0
+
+		while retry_count < data_retry_max and not last :
+			rfds,wfds,efds = select.select( [self.data_sock.fileno()], [], [], float(data_retry_timeout) )
+			if rfds :
+				(buffer,addr) = self.data_sock.recvfrom(1024)
+				try :
+					datapkt = tftp.parse( buffer )
+				except tftp.packet_error,err :
+					# ignore bad packets
+					print "Ignoring bad packet from peer:",err.errmsg
+				else :
+					if datapkt.op != tftp.DATA:
+						print "Bad packet from peer (expected DATA)."
+					elif datapkt.block_num != next_block_num :
+						print "Bad DATA from peer; got block=%d expected block=%d." % (datapkt.block_num,next_block_num)
+					else :
+						# success! 
+						file.write( datapkt.data )
+						ackpkt.block_num += 1
+						self.send( ackpkt )
+
+						next_block_num += 1
+
+						retry_count = 0
+
+						# last packet; we're done
+						if len(datapkt.data) < 512 :
+							last = 1
+				
+			else :
+				print "No response from peer; resending ACK."
+				self.send( ackpkt )
+				retry_count += 1
+
+		if retry_count >= data_retry_max :
+			print "Too many retries.  Giving up."
+
+		self.data_sock.close()
+		file.close()
+
+
+	def read_request( self, tftppkt ) :
+		assert tftppkt.filename
+		assert tftppkt.mode
+
+		self.data_sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+		self.data_sock.bind( ("",0) )
+
+		print self.data_sock.getsockname()
+
+		if not self.pathcheck( tftppkt ) :
+			self.data_sock.close()
+			return
+
+		if not self.filemodecheck( tftppkt ) :
+			self.data_sock.close()
+			return
+
+		try :
+			if tftppkt.mode == "netascii" :
+				file = cvtfile.UnixToDosFile()
+				file.open( tftppkt.filename, "rb" )
+			else :
+				file = open( tftppkt.filename, "rb" )
+
+		except IOError, err :
+			print "Failed to open %s : " % tftppkt.filename, err
+			errpkt = tftp.Error( tftp.ERROR_NO_SUCH_FILE, err.strerror )
+			self.send( errpkt )
+			self.data_sock.close()
+			return
+
+		datapkt = tftp.Data()
+
+		last = 0
+		while not last :
+			datapkt.data = file.read( 512 )
+			if len(datapkt.data) < 512 :
+				# Note that if file size is an exact multiple of 512, we need
+				# to send one more packet of zero data length to indicate end
+				# of transfer.
+				last = 1
+
+			datapkt.block_num += 1
+			self.send( datapkt )
+
+			# wait for the ACK; resend data if we don't get ACK'd
+			retry_count = 0
+			while retry_count < data_retry_max :
+				rfds,wfds,efds = select.select( [self.data_sock.fileno()], [], [], float(data_retry_timeout) )
+				if rfds :
+					(buffer,addr) = self.data_sock.recvfrom(1024)
+					try :
+						ackpkt = tftp.parse( buffer )
+
+						if ackpkt.op != tftp.ACK :
+							print "Bad packet from peer (expected ACK)."
+						elif ackpkt.block_num != datapkt.block_num :
+							print "Bad ACK from peer; got block=%d expected block=%d." % (ackpkt.block_num,datapkt.block_num)
+						else :
+							# success! leave the ACK retry loop
+							break
+					except tftp.packet_error,err :
+						# ignore bad packets
+						print "Ignoring bad packet from peer:",err.errmsg
+				else :
+					print "No response from peer; resending DATA."
+					self.send( datapkt )
+					retry_count += 1
+
+			if retry_count >= data_retry_max :
+				print "Too many retries.  Giving up."
+
+				# leave outer transmit loop
+				last = 1
+
+		file.close()
+		self.data_sock.close()
+
+	def serveit( self ) :
+		try :
+			pkt = tftp.parse( self.request[0] )
+		except tftp.packet_error,err :
+			print err.errmsg
+		else :
+			if pkt.op == tftp.RRQ :
+				self.read_request( pkt )
+			elif pkt.op == tftp.WRQ :
+				self.write_request( pkt )
+			else :
+				print "Ignoring unexpected op."
+
+	def handle( self ) :
+		print "client_address=",self.client_address
+		self.serveit()
+		print "handle() done"
+
+class ReuseUDPServer(SocketServer.UDPServer):
+
+	def server_bind(self) :
+		print "ReuseUDPServer.server_bind()"
+		self.allow_reuse_address = 1
+		SocketServer.UDPServer.server_bind(self)
+
+	def server_activate(self) :
+		print "ReuseUDPServer.server_activate()"
+		SocketServer.UDPServer.server_activate(self)
+
+	def close_request(self,request) :
+		print "ReuseUDPServer.close_request()"
+		SocketServer.UDPServer.close_request(self,request)
+
+tftp.debugging = 1
+
+server = ReuseUDPServer( ( '', tftp.UDP_PORT ), Nettftpd )
+server.serve_forever()
+
